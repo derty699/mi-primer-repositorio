66d28b24b9571b22ed89e024e890d07de2e56e5d
derty699 <danirojo226@gmail.com>
2024-03-18 16:39:00 +0100

    Este es mi primer commit

---
+++ ejercicos 21 en adelante.py
@@ -0,0 +1,17 @@
+num_shows = int(input("¿Cuántos shows musicales has visto en el último año? "))
+
+ha_visto_mas_de_3_shows = bool(num_shows > 3)
+print("¿Has visto más de 3 shows? ", ha_visto_mas_de_3_shows)
+
+
+fecha= int(input("Ingrese una fecha en formato DDMMAAAA: "))
+
+dia= int(fecha / 1000000)
+mes= int(
+
+print(dia,"/",mes,"/",ano)
+
+
+numero = int(input("Ingrese un número entero: "))
+
+print((numero % 2) == 0)
